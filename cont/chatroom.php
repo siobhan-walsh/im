fea5adb5811f61b692adfcfc0251d0ec705c70ee
lahiru <lahiru1218.lm@gmail.com>
2016-02-15 12:33:56 -0800

    add chat room file

---
+++ cont/chatroom.php
@@ -0,0 +1,44 @@
+<?php
+
+include('../model/userdb.php');
+
+    //CRUD users
+
+  if($_POST['method'] == 'insert'){
+      
+      insert_user();
+      
+  }
+
+    if($_POST['method'] == 'getsession'){
+      
+      check_session();
+      
+    }
+
+ if($_POST['method'] == 'getUser'){
+      
+      get_user();
+      
+  }
+
+ if($_POST['method'] == 'showAllUsers'){
+      
+     // showAllUsers();
+      
+  }
+
+if($_POST['method'] == 'updateUser'){
+      
+     // updateUser();
+      
+  }
+
+if($_POST['method'] == 'deleteUser'){
+      
+     // deleteUser();
+      
+  }
+   
+   
+?>
