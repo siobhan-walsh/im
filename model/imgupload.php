7dd22709b457cbab2796b1d8aebf1966b830c08f
siobhan-walsh <siobhanemmarose@gmail.com>
2016-02-23 12:14:20 -0800

    sending img path to db

--- model/imgupload.php
+++ model/imgupload.php
@@ -1,5 +1,9 @@
 <?php
 
+include('connection.php');
+
+    global $db;
+
     //echo 'work';
 
     var_dump($_POST);
@@ -7,6 +11,9 @@
 
     $uid = $_POST['userid'];
     $filename = substr(md5(rand()), 0, 7);
+    $type = explode("/", $_FILES['images']['type'][0])[1];
+    var_dump($type);
+        
 
     if(!is_dir('../img/' . $uid . '/')){
         
@@ -16,11 +23,24 @@
 
    if( move_uploaded_file(
 
-      $_FILES['images']['tmp_name'][0], '../img/'. $uid . '/' . $filename . '.jpg'                               
+      $_FILES['images']['tmp_name'][0], '../img/'. $uid . '/' . $filename . '.' . $type                            
     )) {
        
-       //include db file
+       
          // insert the path to the database ../img/uid/avatar.jpg
+       
+     $path = '../img/'. $uid . '/' . $filename . '.' . $type  ;
+       
+      
+       
+       $query = "UPDATE users SET avi = :avi WHERE user_id = :uid;";
+        
+            $result = $db->prepare($query);
+        
+            $result->execute(array(':uid' => $uid, ':avi'=> $path));
+            
+            echo json_encode('wow');
+        
    };
 
 ?>
